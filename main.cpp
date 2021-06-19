22935a5da38c606ea581179a6fd6728ea433ba57
k4l1brx <19127535@student.hcmus.edu.v>
2021-06-19 22:28:49 +0700

    refactor

--- main.cpp
+++ main.cpp
@@ -1,48 +1,80 @@
 #include <windows.h>
 #include <stdio.h>
 #include <iostream>
+
 #pragma pack(1)
-typedef struct fat_BS
-{
-    unsigned char 		bootjmp[3];
-    unsigned char 		oem_name[8];
-    unsigned short 	        bytes_per_sector;
-    unsigned char		sectors_per_cluster;
-    unsigned short		reserved_sector_count;
-    unsigned char		table_count;
-    unsigned short		root_entry_count;
-    unsigned short		total_sectors_16;
-    unsigned char		media_type;
-    unsigned short		table_size_16;
-    unsigned short		sectors_per_track;
-    unsigned short		head_side_count;
-    unsigned int 		hidden_sector_count;
-    unsigned int 		total_sectors_32;
-    unsigned char		extended_section[54];
+typedef struct fat_BS {
+    unsigned char bootjmp[3];
+    unsigned char oem_name[8];
+    unsigned short bytes_per_sector;
+    uint8_t sectors_per_cluster;
+    unsigned short reserved_sector_count;
+    unsigned char table_count;
+    unsigned short root_entry_count;
+    unsigned short total_sectors_16;
+    unsigned char media_type;
+    unsigned short table_size_16;
+    unsigned short sectors_per_track;
+    unsigned short head_side_count;
+    unsigned int hidden_sector_count;
+    unsigned int total_sectors_32;
     //extended fat32 stuff
-    unsigned int		table_size_32;
-    unsigned short		extended_flags;
-    unsigned short		fat_version;
-    unsigned int		root_cluster;
-    unsigned short		fat_info;
-    unsigned short		backup_BS_sector;
-    unsigned char 		reserved_0[12];
-    unsigned char		drive_number;
-    unsigned char 		reserved_1;
-    unsigned char		boot_signature;
-    unsigned int 		volume_id;
-    unsigned char		volume_label[11];
-    unsigned char		fat_type_label[8];
+    unsigned int sectors_per_fat;
+    unsigned short flags;
+    unsigned short fat_version;
+    unsigned int root_cluster_number;
+    unsigned short fsinfo_sector_number;
+    unsigned short backup_boot_sector;
+    uint8_t reserved[12];
+    uint8_t disk_number;
+    uint8_t __reserved; // Flags for Windows NT, or reserved
+    uint8_t signature;
+    unsigned int volume_id_serial;
+    char volume_label[11];
+    char system_identifier[8];
+    uint8_t boot_code[420];
+    unsigned short bootable_signature;
 } *pFat_BS_T;
 #pragma pack()
 
-int ReadSector(const char*  drive, int readPoint, BYTE sector[512])
-{
-    int retCode = 0;
-    DWORD bytesRead;
-    HANDLE device = NULL;
+HANDLE device;
 
-    device = CreateFile(drive,    // Drive to open
+pFat_BS_T pFat_boot;
+uint8_t* pFat;
+
+bool readBootSector(HANDLE device) {
+    pFat_boot = (pFat_BS_T) malloc(512);
+    DWORD bytesRead;
+    SetFilePointer(device, 0, NULL, FILE_BEGIN);
+    return ReadFile(device, pFat_boot, 512, &bytesRead, NULL);
+}
+
+bool readSector(HANDLE device, int readPoint, int sectorCount, uint8_t* sector) {
+    DWORD bytesRead;
+    int retCode = 0;
+    if (device == INVALID_HANDLE_VALUE) {
+        printf("Error: %u\n", GetLastError());
+        return 0;
+    }
+    SetFilePointer(device, readPoint, NULL, FILE_BEGIN);//Set a Point to Read
+    if (!ReadFile(device, sector, sectorCount * pFat_boot->bytes_per_sector, &bytesRead, NULL)) {
+        //printf("ReadFile: %u\n", GetLastError());
+        return 0;
+    } else {
+        //printf("Success!\n");
+        return 1;
+    }
+}
+
+
+
+bool readFat(HANDLE device) {
+    pFat = (uint8_t*) malloc(pFat_boot->sectors_per_fat * pFat_boot->bytes_per_sector);
+    return readSector(device, pFat_boot->__reserved, pFat_boot->bytes_per_sector, (BYTE*) pFat);
+}
+
+int main(int argc, char **argv) {
+    device = CreateFile("\\\\.\\F:",    // Drive to open
                         GENERIC_READ,           // Access mode
                         FILE_SHARE_READ | FILE_SHARE_WRITE,        // Share Mode
                         NULL,                   // Security Descriptor
@@ -50,38 +82,18 @@ int ReadSector(const char*  drive, int readPoint, BYTE sector[512])
                         0,                      // File attributes
                         NULL);                  // Handle to template
 
-    if (device == INVALID_HANDLE_VALUE) // Open Error
-    {
-        printf("CreateFile: %u\n", GetLastError());
-        return 1;
-    }
-
-    SetFilePointer(device, readPoint, NULL, FILE_BEGIN);//Set a Point to Read
-
-    if (!ReadFile(device, sector, 512, &bytesRead, NULL))
-    {
-        printf("ReadFile: %u\n", GetLastError());
-    }
-    else
-    {
-        printf("Success!\n");
-    }
-}
-
-void ReadBootSector() {
-
-}
-uint16_t swap_uint16( uint16_t val )
-{
-    return (val << 8) | (val >> 8 );
-}
-int main(int argc, char ** argv)
-{
-    BYTE sector[512];
-    ReadSector("\\\\.\\F:",0, sector);
-    pFat_BS_T pFat_boot = (pFat_BS_T) sector;
+    if (! readBootSector(device)) {
+        std::cout << "Failed to read disk\n";
+        return -1;
+    } else
+        if (pFat_boot->bootable_signature == 0xAA55)
+            std::cout << "Bootable signature found ! Success!!!\n";
     std::cout << pFat_boot->oem_name << '\n';
-    std::cout << pFat_boot->bytes_per_sector << '\n';
-    std::cout << (int) pFat_boot->sectors_per_cluster;
+    std::cout << (int) pFat_boot->sectors_per_fat << '\n';
+    if (! readFat(device)) {
+        std::cout << "Reading FAT failed!!";
+        return -1;
+    }
+
     return 0;
 }
